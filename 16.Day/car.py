ff32df227d92057387014e47e398e76af2b64541
root <root@DESKTOP-N5IVS85.localdomain>
2023-08-07 23:53:01 +0600

    Day_16

---
+++ 16.Day/car.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+timmy.color("green")
+timmy.shape("turtle")
+timmy.forward(100)
+
+
+my_screen = Screen()
+my_screen.canvheight = 200
+my_screen.canvwidth = 200
+my_screen.exitonclick()
+
